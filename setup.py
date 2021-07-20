f9202e350156e8c9b91cce32be6aa4ba47aefb9a
mkpark2017 <30460786+mkpark2017@users.noreply.github.com>
2021-07-20 18:28:10 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='gym_ste',
+    version='0.0.3',
+    url='https://github.com/mkpark2017/gym-ste',
+    Author='Minkyu Park',
+    packages=['gym_ste'],
+    install_requires=['gym>=0.18.3','numpy>=1.21.0'],
+    description='OpenAI GYM environments for source term estimation and sample learning codes',
+)
