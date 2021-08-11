1211906308dfc7d77a553a01c03d1cd648aa7ea3
mkpark2017 <30460786+mkpark2017@users.noreply.github.com>
2021-08-11 17:57:59 +0900

    Update ste_est_base.py

--- gym_ste/envs/ste_est_base.py
+++ gym_ste/envs/ste_est_base.py
@@ -18,6 +18,15 @@ class SteEstBaseEnv(SteBaseEnv):
         self.obs_high_state = np.array([1, 20,  self.max_step, self.court_lx, self.court_ly, 1, self.conc_max, self.conc_max, self.conc_max])
         self.observation_space = spaces.Box(self.obs_low_state, self.obs_high_state, dtype=np.float32)
 
+#       [agent angle, source probability]
+        self.action_angle_low = np.array([-1, 0])
+        self.action_angle_high = np.array([1, 1])
+        self.action_space = spaces.Box(self.action_angle_low, self.action_angle_high, dtype=np.float32)
+        
+        self.eps = 0.1*self.court_lx
+        self.conc_eps = 0.2 # minimum conc
+        self.last_highest_conc = self.conc_eps
+        self.normalization = True
         self.seed()
 #        self.reset()
 
@@ -31,7 +40,8 @@ class SteEstBaseEnv(SteBaseEnv):
     def _info_function(self, norm_obs, action, done, rew):
         if self.normalization:
            info = "time step:" + str(self.count_actions) + ", act:" + str(
-                   round(float(action)*180,2)) + ", local flow: (" + str(round(norm_obs[0]*180,2)) + "degree, " + str(
+                   round(float(action[0])*180,2)) + ", source_probability: " + str(
+                   round(float(action[1]),2))+ ", local flow: (" + str(round(norm_obs[0]*180,2)) + "degree, " + str(
                    round(norm_obs[1]*20,2)) + "), duration time: " + str(round(norm_obs[2]*self.max_step,2)) + "Current pos: (" + str(
                    round(norm_obs[3]*self.court_lx,2)) + ", " + str(round(norm_obs[4]*self.court_ly,2)) + "), last action: " + str(
                    round(float(norm_obs[5])*180/math.pi,2)) + ", last conc:" + str(round(norm_obs[6]*self.conc_max,2)) + ", conc:" + str(
@@ -54,26 +64,24 @@ class SteEstBaseEnv(SteBaseEnv):
     def step(self, action):
         self.outborder = False
         self.count_actions += 1
-        self._calculate_position(action)
+        self._calculate_position(action[0])
         obs = self._observation()
 
-        self.last_action = action
+        self.last_action = action[0]
 
         rew = 0
         if self.outborder: # Agent get out to search area
             rew += self._border_reward()
         # done for step rewarding
-        done = bool(self._distance(self.agent_x, self.agent_y) <= self.eps)
+        done = bool(agent[1] > 0.5)
         if not done:
             rew += self._step_reward()
         else: # Reach the source
-            rew = self._reward_goal_reached()
-#        done = bool(self.count_actions >= self.max_step and self._distance(self.agent_x, self.agent_y) > self.eps)
-#        if done: # Reach the max_step without finding source
-#            rew = self._reward_failed()
+            if bool(self._distance(self.agent_x, self.agent_y) <= self.eps)
+                rew = self._reward_goal_reached()
+
         # break if more than max_step actions taken
-        done = bool(self.count_actions >= self.max_step or self._distance(self.agent_x, self.agent_y) <= self.eps or self.outborder)
-#        done = bool(self.count_actions >= self.max_step or self._distance(self.agent_x, self.agent_y) <= self.eps)
+        done = bool(self.count_actions >= self.max_step or agent[1] > 0.5 or self.outborder)
 
         # track, where agent was
         self.positions.append([self.agent_x, self.agent_y])
@@ -93,6 +101,3 @@ class SteEstBaseEnv(SteBaseEnv):
         else:
            info = self._info_function(obs, action, done, rew)
            return obs, rew, done, info
-
-
-
